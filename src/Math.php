f49665d951d41c8889aab1bf02da0b3efad14355
Yur <sergiy.yur@decimadigital.com>
2023-03-11 17:44:35 +0200

    Fix test

--- src/Math.php
+++ src/Math.php
@@ -6,7 +6,7 @@ class Math
 {
     public function sum(int|float $a, int|float $b): int|float
     {
-        return $a + $b + 1;
+        return $a + $b;
     }
 
     public function hello(): string
