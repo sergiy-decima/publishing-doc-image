d8fadd3c30fc95a01dd00911fd0cae8ea3c69f4f
Yur <sergiy.yur@decimadigital.com>
2023-03-11 17:20:06 +0200

    Update Unit Test Results

--- src/Math.php
+++ src/Math.php
@@ -6,7 +6,7 @@ class Math
 {
     public function sum(int|float $a, int|float $b): int|float
     {
-        return $a + $b;
+        return $a + $b + 1;
     }
 
     public function hello(): string
