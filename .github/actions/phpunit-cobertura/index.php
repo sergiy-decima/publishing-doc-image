8e87dcd04deedcb2eb412d4d0e9597ba41c9a949
Yur <sergiy.yur@decimadigital.com>
2023-03-13 22:15:24 +0200

    Test custom action

--- .github/actions/phpunit-cobertura/index.php
+++ .github/actions/phpunit-cobertura/index.php
@@ -5,6 +5,8 @@ $inputFile = $argv[1];
 $inputPercent = (float)$argv[2];
 $inputFailBuildOnFailure = filter_var($argv[3], FILTER_VALIDATE_BOOLEAN);
 
+echo "The Best Test!";
+
 $context = file_get_contents($inputFile);
 
 echo $context;
